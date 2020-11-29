bf4283a94404a2e15d56f4aca85652dbcb51e823
Matheus <matheuscosta2006@gmail.com>
2020-11-29 18:52:41 -0300

    Finish last lesson

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,36 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const foundAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return foundAppointment || null;
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+}
+
+export default AppointmentsRepository;
